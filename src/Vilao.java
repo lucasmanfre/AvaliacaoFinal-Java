ac1fdda753a050469188a08ed59bba16ab537c3e
FelipeRVNK <felipervnk@gmail.com>
2024-06-21 12:41:33 -0300

    add classe combate e alterando variavel raça para  poder do mal ao vilao

--- src/Vilao.java
+++ src/Vilao.java
@@ -1,27 +1,28 @@
 public class Vilao extends Personagem{
 
-    private String raca;
+    private int poderDoMal;
 
     public Vilao() {
-
+        
     }
-
-    public Vilao(String nome, String classe, int nivel, int pontosVida, int forca, String raca) {
+    
+    public Vilao(String nome, String classe, int nivel, int pontosVida, int forca, int poderDoMal) {
         super(nome, classe, nivel, pontosVida, forca);
-        this.raca = raca;
+        this.poderDoMal = poderDoMal;
+    }
+    
+    public int getPoderDoMal() {
+        return poderDoMal;
     }
 
-    public String getRaca() {
-        return raca;
+    public void setPoderDoMal(int poderDoMal) {
+        this.poderDoMal = poderDoMal;
     }
-
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
+    
 
     @Override
     public String toString() {
-        return super.toString() + ", raca=" + raca;
+        return super.toString() + ", poderDoMal=" + poderDoMal;
     }
 
     public void fromString(String linha){
@@ -32,13 +33,14 @@ public class Vilao extends Personagem{
         setNivel(Integer.parseInt(partes[2].split("=")[1]));
         setPontosVida(Integer.parseInt(partes[3].split("=")[1]));
         setForca(Integer.parseInt(partes[4].split("=")[1]));
-        setRaca(partes[5].split("=")[1]);
+        setPoderDoMal(Integer.parseInt(partes[5].split("=")[1]));
     }
 
     @Override
     public String exibirDados() {
         return super.exibirDados() + 
-        "\nRaça:" + raca;
+        "\nPoder do mal:" + poderDoMal;
     }
+
   
 }
